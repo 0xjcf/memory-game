5d4b756f04c042013480928b74e030a2cfb7f0ff
jose123 <jose@test.com>
2018-12-09 20:59:51 -0600

    add timers to open and hide cards

--- js/app.js
+++ js/app.js
@@ -12,7 +12,10 @@ const listOfCards = [
 
 // Keep count of moves
 let moveCount = 0;
+// List of cards that are flipped and open
 let openList = [];
+// Multiply list by two and merge lists
+const shuffledList = shuffle([...listOfCards, ...listOfCards]);
 
 // Shuffle list of cards
 function shuffle(array) {
@@ -30,9 +33,6 @@ function shuffle(array) {
 
   return array;
 }
-
-// Multiply list by two and merge lists
-const shuffledList = shuffle([...listOfCards, ...listOfCards]);
 
 // Create fragment for cards to append to
 const fragment = document.createDocumentFragment();
@@ -88,20 +88,24 @@ function addToOpenList(cardName) {
 
 function keepCardsOpen(openCards) {
   for (const card of openCards) {
-    card.classList.add("match");
+    setTimeout(function() {
+      card.classList.add("match");
+    }, 2000);
   }
 }
 
 function hideCards(openCards) {
   for (const card of openCards) {
-    card.classList.remove("show", "open");
+    setTimeout(function() {
+      card.classList.remove("show", "open");
+    }, 2000);
   }
 }
 
 function compareCards() {
   const openCards = deck.querySelectorAll(".show, .open");
   if (openList[0] === openList[1]) {
-    keepCardsOpen(openCards);
+    skeepCardsOpen(openCards);
   } else {
     hideCards(openCards);
   }
@@ -111,12 +115,5 @@ function compareCards() {
 deck.addEventListener("click", e => clickHandler(e));
 
 /*
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *
- * + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *
- * + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
